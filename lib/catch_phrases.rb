82f9169ce4f9f8e00a16ed39d0a16a5ec3758bff
Kosi Akporji <kosiazom@gmail.com>
2020-04-13 22:03:25 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,7 +14,7 @@ def link
 end
 
 def all_phrases
-puts "It's-a me, Mario!"
+ puts "It's-a me, Mario!"
 puts 'Thank You Mario! But Our Princess Is In Another Castle!'
 puts "It's Dangerous To Go Alone! Take This."
 end
